cec00a012c6071224607643effc196033597f99e
Jack Conway <j124519951@gmail.com>
2020-01-07 09:14:43 -0500

    u

--- src/Repository/BaseUploadHandler.php
+++ src/Repository/BaseUploadHandler.php
@@ -482,14 +482,16 @@ class BaseUploadHandler
         while(is_dir($this->get_upload_path($name))) {
             $name = $this->upcount_name($name);
         }
-        // Keep an existing filename if this is part of a chunked upload:
-        $uploaded_bytes = $this->fix_integer_overflow($content_range[1]);
-        while(is_file($this->get_upload_path($name))) {
-            $file_size = $this->get_file_size($this->get_upload_path($name));
-            if ($uploaded_bytes === $file_size) {
-                break;
+        if(isset($content_range)){
+            // Keep an existing filename if this is part of a chunked upload:
+            $uploaded_bytes = $this->fix_integer_overflow($content_range[1]);
+            while(is_file($this->get_upload_path($name))) {
+                $file_size = $this->get_file_size($this->get_upload_path($name));
+                if ($uploaded_bytes === $file_size) {
+                    break;
+                }
+                $name = $this->upcount_name($name);
             }
-            $name = $this->upcount_name($name);
         }
         return $name;
     }
